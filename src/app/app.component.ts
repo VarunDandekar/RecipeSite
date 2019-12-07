88760e3dc1e37a0661ffda172cbfb0b93e05c59b
VarunDandekar <varundandekar95@gmail.com>
2019-12-07 10:58:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rcpSite';
+  feature:string = 'recipe';
+
+  onNavigate($event:string){
+    this.feature = $event;
+  }
 }
